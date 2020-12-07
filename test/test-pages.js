058f16bedf32408c7dfebba99cc2f85828993f93
Herbert Rafael Reyes Portillo <36286254+herbertreyes13j@users.noreply.github.com>
2020-12-06 19:20:55 -0600

    Prueba 4

--- test/test-pages.js
+++ test/test-pages.js
@@ -6,7 +6,7 @@ const funciones = require('../funciones')
 describe("validator isNumValid()", () => {
 
 	it("Deberia retornar verdadero si el numero es mayor a 0", ()=> {
-		expect(funciones.EsPositivo(43)).to.be.true
+		expect(funciones.EsPositivo(463)).to.be.true
 	})
 
 	it("Deberia retornar falso si el numero es menor a 0", () => {
